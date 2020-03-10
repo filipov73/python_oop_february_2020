d7ae657bfaa60ac8669f3e4c706e0eced9b89a7d
MarioFilco <m_filipov@yahoo.com>
2020-03-10 09:04:55 +0200

    start

--- 06.Classes and Instances - Exercise/05. Time.py
+++ 06.Classes and Instances - Exercise/05. Time.py
@@ -9,14 +9,24 @@ class Time:
         self.seconds = int(seconds)
 
     def set_time(self, hours, minutes, seconds):
-        self.hours += hours
-        self.minutes += minutes
-        self.seconds += seconds
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
 
     def get_time(self):
         return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
 
     def next_second(self):
+        self.seconds += 1
+        if self.seconds >= self.max_seconds:
+            self.seconds = 0
+            self.minutes += 1
+            if self.minutes >= self.max_minutes:
+                self.minutes = 0
+                self.hours += 1
+                if self.hours >= self.max_hours:
+                    self.hours = 0
+
         return self.get_time()
 
 """
